3a4ad9f054bd15a64bdef68928b74c0f23ef32d7
SubhanKhurshid <105592966+SubhanKhurshid@users.noreply.github.com>
2024-02-18 16:35:52 +0500

    final

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -4,8 +4,9 @@ import { IEvent } from "@/lib/database/models/event.model";
 import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "../ui/button";
-import Link from "next/link";
+
 import Checkout from "./Checkout";
+import Link from "next/link";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
   const { user } = useUser();
